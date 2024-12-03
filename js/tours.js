6733062c50cc15c493479f130c0559093b40251e
JordanJamesJA <97801730+JordanJamesJA@users.noreply.github.com>
2024-12-03 02:38:09 -0500

    Fixed minor nav issue for tours page.

--- js/tours.js
+++ js/tours.js
@@ -127,3 +127,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("show-menu");
+    toggle.classList.toggle("show-icon");
+  });
+};
+
+showMenu("nav-toggle", "nav-menu");
